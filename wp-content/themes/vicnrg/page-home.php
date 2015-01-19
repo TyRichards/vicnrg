182042c264a9ffee758c04c627812309943f0972
Ty Richards <me@tyrichards.com>
2015-01-19 14:09:46 -0600

    Home page revisions

--- wp-content/themes/vicnrg/page-home.php
+++ wp-content/themes/vicnrg/page-home.php
@@ -11,13 +11,13 @@ Template Name: Home Template
         <div class="lg-logo col-xs-12 col-md-12">
           <img class="img-responsive text-center" src="<?php echo get_template_directory_uri(); ?>/library/images/logo-blue.svg" alt="VicNRG" width="100%" />
         </div>
-        <div class="col-xs-10 col-xs-offset-1 col-md-12 col-md-offset-0">
+        <!-- <div class="col-xs-10 col-xs-offset-1 col-md-12 col-md-offset-0">
 	        <p class="lead text-center"><i>In the quest for energy independence, VicNRG is the only real solution.</i></p>
-        </div>
-        <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
-	        	<!-- <a href="../contact" type="button" class="btn btn-block btn-trans btn-xl">Contact Us</a> -->
+        </div> -->
+        <!-- <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
+	        	<a href="../contact" type="button" class="btn btn-block btn-trans btn-xl">Contact Us</a>
 	        	<a type="button" class="btn btn-block btn-trans btn-xl visible-xs">817.562.4888</a>        	
-        </div>
+        </div> -->
       </div>
     </div>
 
